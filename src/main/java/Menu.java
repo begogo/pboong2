0c594f627543737d07dc053c20e668ec7af37744
gongbu <asd@asd.com>
2021-04-13 05:30:08 +0900

    feat: '공부하기' 보완, '휴식하기' 추가 style: 변수명, 메소드명 등 기초 컨벤션 준수

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
+/**
+ * 객체지향 생각 안하고 단순하게 만들었습니다. 클래스간 변수 공유할 때 static 쓰면 안좋지만 일단 사용..
+ */
+
 public class Menu {
 
     static Scanner scan = new Scanner(System.in);
+    static int condition = 100;
+    static int money = 1000000;
+    static int java = 0;
+    static int csKnowledge = 0;
+    static int codingTest = 0;
+    static int day = 1;
 
     public static void main(String[] args) {
 
@@ -10,14 +20,13 @@ public class Menu {
 
             System.out.println("_____________________________________________");
             System.out.println("|               ㅡ Welcome to ㅡ             |");
-            System.out.println("|               프붕이 취직하기 고!            |");
+            System.out.println("|               프붕이 취직하기 고!           |");
             System.out.println("|*******************************************|");
             System.out.println("|                                           |");
-            System.out.println("|   원하시는 메뉴번호를 입력 후 엔터를 눌러주세요   |");
+            System.out.println("|  원하시는 메뉴번호를 입력 후 엔터를 눌러주세요 |");
             System.out.println("|                                           |");
-            System.out.println("| 1) 시작하기                                 |");
-            System.out.println("| 2) 종료하기                                 |");
-            System.out.println("|                                           |");
+            System.out.println("| 1) 시작하기                                |");
+            System.out.println("| 2) 종료하기                                |");
             System.out.println("|___________________________________________|");
             System.out.print("입력>>>");
 
@@ -26,7 +35,7 @@ public class Menu {
             switch (menu) {
                 case 1:
                     System.out.println("프붕이는 개발자가 되고자 마음먹었습니다.");
-                    dayBegin();
+                    begin();
                     break;
                 case 2:
                     System.out.println("종료합니다.");
@@ -40,36 +49,29 @@ public class Menu {
 
     }
 
+    static void begin(){
 
-
-    static void dayBegin(){
-
-        int condition = 100;
-        int money = 1000000;
-        int java = 0;
-        int csKnowledge = 0;
-        int codingTest = 0;
-
-        for(int n=1; n>-1; n++){
+        while(true){
 
             System.out.println("*************************************************");
             System.out.println("현재상태");
             System.out.println("컨디션: "+ condition + "          자금: " + money);
-            System.out.println("자바 스킬: " + java + "         cs지식: " + csKnowledge);
+            System.out.println("자바 스킬: " + java + "         CS지식: " + csKnowledge);
             System.out.println("코딩테스트 능력: " + codingTest);
             System.out.println("*************************************************");
-            System.out.println("Day-" + n + " 오늘은 무엇을 하시겠습니까?");
-            System.out.println("1)공부 2)휴식 3)돈벌기 4)구직활동");
+            System.out.println("Day-" + day + " 오늘은 무엇을 하시겠습니까?");
+            System.out.println("1)공부  2)휴식  3)돈벌기  4)구직활동  5)메인메뉴로(현재상태 저장됨)");
+
             int dayChoice = scan.nextInt();
+            day++;
 
             switch (dayChoice){
 
                 case 1:
-                    java += 5;
-                    System.out.println("자바 스킬이 5점 올랐습니다."); //보충필요
+                    Study.chooseField();
                     break;
                 case 2:
-                    System.out.println("휴식하기"); //미구현
+                    Rest.recover();
                     break;
                 case 3:
                     System.out.println("돈벌기"); //미구현
@@ -77,11 +79,14 @@ public class Menu {
                 case 4:
                     System.out.println("구직활동"); //미구현
                     break;
+                case 5:
+                    return;
                 default:
-                    n--;
+                    day--;
                     System.out.println("잘못된 번호입니다. 다시 입력해주세요.");
                     break;
             }
+
         }
 
     }
---
+++ src/main/java/Moneymaking.java
@@ -0,0 +1,5 @@
+public class Moneymaking {
+
+    //일하기, 로또
+
+}
